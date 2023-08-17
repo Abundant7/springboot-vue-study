304945524a3b1aa7863b638d07c6d9650c541b7c
Abundant7 <3220349251@qq.com>
2023-08-17 18:18:38 +0800

    细节修改

--- springboot_demo/src/main/java/com/example/controller/UserController.java
+++ springboot_demo/src/main/java/com/example/controller/UserController.java
@@ -1,9 +1,15 @@
 package com.example.controller;
 
 
+import com.example.pojo.User;
+import com.example.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +22,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private UserService userService;
+    @GetMapping("/list")
+    public List<User> list(){
+        return userService.list();
+    }
 
 }
